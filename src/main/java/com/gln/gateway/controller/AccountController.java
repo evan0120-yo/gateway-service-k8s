0bdc9055d19caed56174133ddd9432d594f8bcce
ASUS-ZENBOOK-GC\evan <evan01203394@globalline.club>
2026-02-11 15:35:17 +0800

    add log for login

--- src/main/java/com/gln/gateway/controller/AccountController.java
+++ src/main/java/com/gln/gateway/controller/AccountController.java
@@ -9,9 +9,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gln.gateway.dto.LoginDto;
+import com.gln.gateway.dto.LoginVo;
 import com.gln.gateway.model.Account;
 import com.gln.gateway.service.AccountService;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @CrossOrigin(value = "*")
 @RestController
 @RequestMapping(value = "/account")
@@ -22,12 +25,21 @@ public class AccountController {
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody LoginDto loginDto) {
-        return ResponseEntity.ok(accountService.loginAccount(loginDto.getUsername(), loginDto.getPassword()));
+        log.info("[LOGIN] Login attempt: username={}", loginDto.getUsername());
+        LoginVo result = accountService.loginAccount(loginDto.getUsername(), loginDto.getPassword());
+        if (result.getAccount() != null) {
+            log.info("[LOGIN] Login success: username={}", loginDto.getUsername());
+        } else {
+            log.warn("[LOGIN] Login failed: username={}", loginDto.getUsername());
+        }
+        return ResponseEntity.ok(result);
     }
 
     @PostMapping("/register")
     public ResponseEntity<?> register(@RequestBody Account account) {
+        log.info("[REGISTER] Register attempt: username={}", account.getUsername());
         accountService.save(account.getUsername(), account.getPassword());
+        log.info("[REGISTER] Register success: username={}", account.getUsername());
         return ResponseEntity.ok().build();
     }
 }
